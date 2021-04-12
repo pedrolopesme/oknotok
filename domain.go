b1ca5c61b473d004be443c8cc842dcfd120cc792
Pedro Mendes <pedrolopesme@gmail.com>
2021-04-12 11:09:42 -0300

    minor: added Settigns and OkNotOk type

--- domain.go
+++ domain.go
@@ -1,6 +1,9 @@
 package oknotok
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // represents the current state of the circuit breaker
 type CircuitState int
@@ -35,4 +38,29 @@ type Stats struct {
 	Successes           uint64
 	ContinuousSuccesses uint64
 	ContinuousFailures  uint64
+}
+
+// general settings applyied to every CircuitBreaker
+type Settings struct {
+	Name         string
+	MaxRequests  uint64
+	Interval     time.Duration
+	Timeout      time.Duration
+	ReadyToTrip  func(stats Stats) bool
+	StateChanged func(name string, from, to CircuitState)
+	IsSuccessful func(err error) bool
+}
+
+// Core domain type, implementing the most important features of a
+// CircuitBreaker. It helps to protect the environment
+// from sending requests that probably are going to fail.
+// Source of inspiration: https://martinfowler.com/bliki/CircuitBreaker.html
+type OkNotOk struct {
+	name         string
+	maxRequests  uint64
+	interval     time.Duration
+	timeout      time.Duration
+	readyToTrip  func(stats Stats) bool
+	stateChanged func(name string, from, to CircuitState)
+	isSuccessful func(err error) bool
 }
--- oknotok.go
+++ oknotok.go
@@ -1,7 +1,1 @@
 package oknotok
-
-const (
-	StateOk int = iota
-	StateHalfOk
-	StateNotOk
-)
