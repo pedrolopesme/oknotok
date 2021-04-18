a80cc62e8b8054ee417eabf9b4f9639b9b6eb97b
Pedro Mendes <pedrolopesme@gmail.com>
2021-04-17 21:28:15 -0300

    minor: add counters to Stats to call/success/failure

--- stats.go
+++ stats.go
@@ -4,19 +4,41 @@ package oknotok
 // TODO clear the internal status on the change of the state and/or at the
 // close state intervals
 type Stats struct {
-	requests            uint64
+	calls               uint64
 	failures            uint64
 	successes           uint64
 	continuousSuccesses uint64
 	continuousFailures  uint64
 }
 
-// clear all internal stats
+// clears all internal stats
 // TODO add tests
 func (s *Stats) reset() {
-	s.requests = 0
+	s.calls = 0
 	s.failures = 0
 	s.successes = 0
 	s.continuousFailures = 0
+	s.continuousSuccesses = 0
+}
+
+// increments a call
+// TODO add tests
+func (s *Stats) onCall() {
+	s.calls++
+}
+
+// increments a success
+// TODO add tests
+func (s *Stats) onSuccess() {
+	s.successes++
+	s.continuousSuccesses++
+	s.continuousFailures = 0
+}
+
+// increments a failure
+// TODO add tests
+func (s *Stats) onFailure() {
+	s.failures++
+	s.continuousFailures++
 	s.continuousSuccesses = 0
 }
